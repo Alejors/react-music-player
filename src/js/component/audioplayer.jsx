c6e6ecdd8c18b424244d6ab9c477a6536474f059
alejo <alejoatria@gmail.com>
2022-07-08 17:51:21 -0400

    se modifica tamaño de botones

--- src/js/component/audioplayer.jsx
+++ src/js/component/audioplayer.jsx
@@ -94,31 +94,31 @@ const Audioplayer = () => {
 			<audio ref={songRef} className="music" autoPlay></audio>
 			<div className="d-flex col-md-12 py-1 bg-dark text-light text-center sticky-bottom">
 				<div className="btn-group m-auto" role="group" aria-label="Basic example">
-					<button onClick={() => prevsong(songRef.current.id)} type="button" className="btn btn-secondary">
+					<button onClick={() => prevsong(songRef.current.id)} type="button" className="btn btn-secondary btn-sm">
 						<i className="mx-2 fa-solid fa-backward-step"></i>
 					</button>
-					<button onClick={() => playorpause()} type="button" className="btn btn-secondary">
+					<button onClick={() => playorpause()} type="button" className="btn btn-secondary btn-sm">
 						{playing === false ?
 							<i className="mx-2 fa-solid fa-play"></i> :
 							<i className="fa-solid fa-pause"></i>
 						}
 					</button>
-					<button onClick={() => nextsong(songRef.current.id)} type="button" className="btn btn-secondary">
+					<button onClick={() => nextsong(songRef.current.id)} type="button" className="btn btn-secondary btn-sm">
 						<i className="mx-2 fa-solid fa-forward-step"></i>
 					</button>
-					<button onClick={() => loop()} type="button" className="btn btn-secondary">
+					<button onClick={() => loop()} type="button" className="btn btn-sm btn-secondary">
 						<i className="fa-solid fa-repeat"></i>
 					</button>
-					<button onClick={() => shuffle()} type="button" className="btn btn-secondary">
+					<button onClick={() => shuffle()} type="button" className="btn btn-secondary btn-sm">
 						<i className="fa-solid fa-shuffle"></i>
 					</button>
 				</div>
 				<div className="btn-group m-auto" role="group" aria-label="Basic example">
-					<button onClick={() => volumedown()} type="button" className="ms-1 btn btn-secondary">
+					<button onClick={() => volumedown()} type="button" className="btn btn-secondary btn-sm">
 						<i className="fa-solid fa-minus"></i>
 					</button>
-					<button className="btn btn-secondary disabled">Volume</button>
-					<button onClick={() => volumeup()} type="button" className="btn btn-secondary">
+					<button className="btn btn-secondary btn-sm disabled">Volume</button>
+					<button onClick={() => volumeup()} type="button" className="btn btn-secondary btn-sm">
 						<i className="fa-solid fa-plus"></i>
 					</button>
 				</div>
